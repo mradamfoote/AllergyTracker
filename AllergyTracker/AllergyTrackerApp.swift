9e37abb6fdedb54e1d8dc163d45fd9eaddb0285b
Adam <mradamfoote@gmail.com>
2024-02-20 12:53:34 -0500

    Initial Commit

---
+++ AllergyTracker/AllergyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyTrackerApp.swift
+//  AllergyTracker
+//
+//  Created by Adam Foote on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AllergyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
